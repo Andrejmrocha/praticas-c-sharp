8cb58bb26fd0cc0ee9a9cdb6d666054e18c41717
andre jonas <ajonasmorais@gmail.com>
2023-10-10 12:06:37 -0300

    student scores console app

---
+++ ConsoleApps/students_scores/Program.cs
@@ -0,0 +1,113 @@
+﻿int examAssignments = 5;
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+
+int[] studentScores = new int[10];
+
+string currentStudentLetterGrade = "";
+
+Console.Clear();
+Console.WriteLine("Student\t\tExame Score\tOverall\tGrade\tExtra Credit\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade = 0;
+
+    int gradedAssignments = 0;
+
+    decimal avgScores = 0;
+
+    decimal extraCredits = 0;
+
+    decimal extra = 0;
+
+    foreach (int score in studentScores)
+    {
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments){
+            sumAssignmentScores += score;
+            avgScores += score;
+        }
+            
+        else{
+            sumAssignmentScores += score / 10;
+            if (gradedAssignments < 7){
+                extra += score;
+            }
+
+            
+            extraCredits = extraCredits + (decimal)(score * 0.1);
+        }
+            
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+    avgScores = (decimal) avgScores/examAssignments;
+    decimal pts = (decimal)extraCredits/5;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{avgScores}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{pts} pts");
+}
+
+Console.WriteLine("\n\rPress the Enter key to continue");
+Console.ReadLine();
